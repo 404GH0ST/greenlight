1c577c98ded7606faa8f24153730b5f316b48d80
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2024-12-15 15:25:17 +0700

    feat: add MovieModel

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/404GH0ST/greenlight/internal/data"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -31,6 +32,7 @@ type config struct {
 type application struct {
 	config config
 	logger *slog.Logger
+	models data.Models
 }
 
 func main() {
@@ -75,6 +77,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
