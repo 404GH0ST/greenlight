105f4d67e9a0258797408648e3f3bd766a5be473
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2024-12-31 23:02:27 +0700

    feat: implement permissions

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"slices"
 	"time"
+
+	"github.com/lib/pq"
 )
 
 type Permissions []string
@@ -21,7 +23,7 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 	query := `
   SELECT permissions.code
   FROM permissions
-  INNER JOIN user_permissions ON user_permissions.permission_id = permissions.id
+  INNER JOIN users_permissions ON users_permissions.permission_id = permissions.id
   INNER JOIN users ON users_permissions.user_id = users.id
   WHERE users.id = $1`
 
@@ -53,4 +55,16 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 	}
 
 	return permissions, nil
+}
+
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `
+  INSERT INTO users_permissions
+  SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, userID, pq.Array(codes))
+	return err
 }
