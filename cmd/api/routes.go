4058540137d6b53f03f428fbcc08894c0f5a4067
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2025-01-01 11:33:43 +0700

    feat: add token request

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -48,6 +48,7 @@ func (app *application) routes() http.Handler {
 		"/v1/tokens/authentication",
 		app.createAuthenticationTokenHandler,
 	)
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/activation", app.createActivationTokenHandler)
 	router.HandlerFunc(
 		http.MethodPost,
 		"/v1/tokens/password-reset",
